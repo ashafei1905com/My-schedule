7f337a6b46fcfa0f37a77810d43a8515c5ae41ee
ashafei1905com <ashafei1905@gmail.com>
2026-08-02 19:07:00 +0300

    WWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWW

--- _readlines.ps1
+++
@@ -1,8 +0,0 @@
-param([int]$Start, [int]$End)
-$lines = Get-Content -LiteralPath 'c:/html6/My-schedule/index.html'
-$end = if($End -le 0){ $lines.Length } else { $End }
-if($end -gt $lines.Length){ $end = $lines.Length }
-for($i=$Start; $i -lt $end; $i++){
-  '{0}:{1}' -f ($i+1), $lines[$i]
-}
-
